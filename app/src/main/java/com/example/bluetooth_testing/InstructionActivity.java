b46e69bfe977abb1ee2d93afaf9a1993e56ddd35
kangsheng <1801595H@student.tp.edu.sg>
2020-08-19 12:05:30 +0800

    Instructions update

--- app/src/main/java/com/example/bluetooth_testing/InstructionActivity.java
+++ app/src/main/java/com/example/bluetooth_testing/InstructionActivity.java
@@ -75,7 +75,7 @@ public class InstructionActivity extends Activity
             @Override
             public void onClick(View v) {
 
-                gifImagePath  = "<body><center><img src = \"file:///android_res/drawable/PianoTutorial.gif\"/></center></body>";
+                gifImagePath  = "<body><center><img src = \"file:///android_res/drawable/PianoTutoria.gif\"/></center></body>";
                 webView.loadDataWithBaseURL("file:///android_asset/",gifImagePath,"text/html","UTF-8",null);
                 instructionText.setText("Follow the highlighted piano bars and bend your respective finger to play the game");
             }
